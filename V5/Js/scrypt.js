5d609f8a1a69ca209c144a15fff9aeaa2a52688c
officialCaesardev <116649209+officialCaesardev@users.noreply.github.com>
2022-12-05 04:01:08 +0530

    Add files via upload

---
+++ V5/Js/scrypt.js
@@ -0,0 +1,7 @@
+const toggle = document.getElementById('toggle-js');
+const body = document.querySelector('body');
+toggle.addEventListener('click', function ()){
+    if (
+        this.classlist
+    )
+}
